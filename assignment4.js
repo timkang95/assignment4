baa653ae4a201fd4cac10ddbcf3f5b56131e811f
Timothy Kang <timothykang8@gmail.com>
2016-12-01 21:56:55 -0500

    Timothy Kang Assignment 4

--- assignment4.js
+++ assignment4.js
@@ -11,6 +11,38 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
+$(document).ready(function(){
+  var data = [];
+  $.ajax({
+    url: 'http://www.mattbowytz.com/simple_api.json?data=all',
+    dataType: 'json',
+  }).done(function(ret){
+    for(item in ret.data.programming) data.push(ret.data.programming[item].toLowerCase());
+    for(item in ret.data.interests) data.push(ret.data.interests[item].toLowerCase());
+  });
+
+  $(".flexsearch-input").keyup(function(event){
+    var search = $(".flexsearch-input").val() || "";
+    search = search.toLowerCase();
+    if(search === ""){
+      //do nothing
+    }else{
+       googleSearch(search);
+    }
+  });
+
+  //google search
+  function googleSearch(search){
+    var list = "";
+    for(item in data){
+      if( data[item].substring(0, search.length) === search ) {
+        list += ( "<li><a href='http://www.google.com/search?q="+data[item]+"' target='_blank'>" + data[item] + "</a></li>" );
+      }
+    }
+    $("#resultingList").html(list);
+  }
+});
+
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
